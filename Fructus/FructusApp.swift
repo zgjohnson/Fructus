275ba2b2ac93a80c9a955d7ef1a5fc79057a3db4
Zach Johnson <zgjohnson94@gmail.com>
2023-02-03 20:37:19 -0600

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Zach Johnson on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
