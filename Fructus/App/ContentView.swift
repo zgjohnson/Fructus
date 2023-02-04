0ef7346c88489fa8fe826efb9d036bf691cfb6b5
Zach Johnson <zgjohnson94@gmail.com>
2023-02-04 14:16:17 -0600

    Lecture 5

--- Fructus/App/ContentView.swift
+++ Fructus/App/ContentView.swift
@@ -8,20 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTIES
+    
+    var fruits: [Fruit] = fruitsData
+    
+    // MARK: - BODY
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+            List {
+                ForEach(fruits.shuffled()) { fruit in
+                    FruitRowView(fruit: fruit)
+                        .padding(.vertical, 4)
+                }
+            }
+            .navigationTitle("Fruits")
+        } //: NAVIGATION
     }
 }
 
+// MARK: - PREVIEW
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
-            .preferredColorScheme(.dark)
+        ContentView(fruits: fruitsData)
     }
 }
